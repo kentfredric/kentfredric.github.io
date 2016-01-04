5ec6707bcb1fb0758bfdba3660c30ae2e8c224be
Kent Fredric <kentfredric@gmail.com>
2016-01-05 00:27:18 +1300

    Fixup headings code

--- lib/KENTNL/Heading.pm
+++ lib/KENTNL/Heading.pm
@@ -20,12 +20,14 @@ sub heading {
         $copy =~ s/\W+/_/g;
         $copy;
       };
-    my @hypers = (
+    my @inner = (
+        $args->{site}->markdown->_RunSpanGamut($name)
+        ,    # $args->{page}->{template}->_template->escape->($name)),
         qq[<a class="toplink" href="#top">^</a>],
         qq[<a class="permalink" href="\#$id">\x{2693}</a>],
     );
     return sprintf '<%1$s id="%2$s">%3$s</%1$s>', $heading, $id, join q[],
-      ( $name, @hypers );
+      @inner;
 }
 
 sub footnote {
