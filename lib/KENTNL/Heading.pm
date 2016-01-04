ac1f164b05ce21079a9f5e2174b994d4977b0e57
Kent Fredric <kentfredric@gmail.com>
2016-01-04 23:25:44 +1300

    Add footnote support

--- lib/KENTNL/Heading.pm
+++ lib/KENTNL/Heading.pm
@@ -28,9 +28,34 @@ sub heading {
       ( $name, @hypers );
 }
 
+sub footnote {
+    my ( $self, $args, $text ) = @_;
+    my $footnote_stash = ( $args->{self}->{x_footnotes} ||= [] );
+    my $human_no = 1 + scalar @{$footnote_stash};
+    push @{$footnote_stash}, $text;
+    return
+qq[<a class="footnote_ref" id="from_footnote_${human_no}" href="#footnote_${human_no}">$human_no</a>];
+}
+
+sub footnotes {
+    my ( $self, $args, ) = @_;
+    my $footnote_stash = ( $args->{self}->{x_footnotes} ||= [] );
+    my @out;
+    for my $footnote_no ( 0 .. $#{$footnote_stash} ) {
+        my $human_no = $footnote_no + 1;
+        push @out,
+qq[<p class="footnote"><a id="footnote_${human_no}" href="#from_footnote_${human_no}">${human_no}.</a> ]
+          . $footnote_stash->[$footnote_no]
+          . qq[</p>];
+    }
+    return join q[], @out;
+}
+
 sub register {
     my ( $self, $site ) = @_;
-    $site->theme->helper( heading => sub { $self->heading(@_) } );
+    $site->theme->helper( heading   => sub { $self->heading(@_) } );
+    $site->theme->helper( footnote  => sub { $self->footnote(@_) } );
+    $site->theme->helper( footnotes => sub { $self->footnotes(@_) } );
 }
 1;
 
